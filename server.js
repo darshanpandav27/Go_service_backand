5cab0275f6cf5962eeb9aed344be2a0ad7f1c0a5
Danny <135806067+dannydeniel24@users.noreply.github.com>
2023-09-15 19:31:11 +0530

    api done

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('DB connected...');
 });
-
-// global.appRoot = path.resolve(__dirname);
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
